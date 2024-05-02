bca53b24869241a3c49678d1bd36c991e9caf822
Sachin7717 <sachindhimann71@gmail.com>
2024-05-02 09:45:19 +0530

    importing react in other file

---
+++ src/module1.mjs
@@ -0,0 +1,9 @@
+const a  ='apple';
+const b ='banana';
+const c='grape';
+const d='cherry';
+
+export default b;
+export {a};
+export {c};
+export {d};
---
+++ src/module2.mjs
@@ -0,0 +1,4 @@
+import ui,{a,c,d} from './module1.mjs'
+console.log(ui);
+console.log(a,c,d)
+
